3fd1e1ddb9bc35474989f74758ee2aa5398b4f49
JackyGAMA <98359862+JackyGAMA@users.noreply.github.com>
2023-05-05 23:05:34 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package org.exmaple;
+
+public class Main{
+public static void main(String[] args){
+    System.out.println("Hello world");
+    Scanner scanner = new Scanner(System.in);
+
+
+
+   
+
+
+    try{
+        int a = 10;
+        int b = Integer.parseInt(scanner.nextLine());
+        System.out.println("Division:" +(a/b));
+    }catch(ArithmeticException e){
+         System.out.println("No se puede dividir entre 0");
+        } catch (NumberFormatException nfe){
+            System.out.println("no capturaste el dato correctamente";)
+        }finally{
+          System.out.println("siempre se ejecuta...");
+        }   
+      System.out.println("fin del programa");
+
+     }
+}
